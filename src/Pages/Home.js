e599f193fe0f9c945f1b83d56733305ec88e6414
Johnny1305 <jonathan.ramos.business@gmail.com>
2023-11-11 13:15:11 +0100

    Updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ const HomeContent = () => {
     <div className="home-content">
       <div className="logo-circle">
         <img
-          src="https://numberbot.johnnyportfolio.x10.mx/logo.png" // Reemplaza con la ruta correcta de tu logo
+          src="https://cdn.discordapp.com/attachments/1136971388545142965/1172871785344155731/standard.gif?ex=6561e4f8&is=654f6ff8&hm=5b52375c9a73a9cda9430cc559ff3306cd8634bc67b575e2fd478fb366ab0354&" // Reemplaza con la ruta correcta de tu logo
           alt="Logo del bot"
           className="bot-logo"
         />
--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -317,7 +317,7 @@ const Sidebar = () => {
         <Profile clicked={profileClick}>
           <img
             onClick={() => handleProfileClick()}
-            src="https://numberbot.johnnyportfolio.x10.mx/logo.png"
+            src="https://cdn.discordapp.com/attachments/1136971388545142965/1172871785344155731/standard.gif?ex=6561e4f8&is=654f6ff8&hm=5b52375c9a73a9cda9430cc559ff3306cd8634bc67b575e2fd478fb366ab0354&"
             alt="Profile"
           />
           <Details clicked={profileClick}>
