eaa6a873201b588d4f77ff7e706aaa723aa5c672
Johnny1305 <jonathan.ramos.business@gmail.com>
2023-11-17 14:31:24 +0100

    Update ChangeLog.js
    Se actualiza los datos de la nueva versión del Bot

--- src/Pages/ChangeLog.js
+++ src/Pages/ChangeLog.js
@@ -3,7 +3,7 @@ import MotionHoc from "./MotionHoc";
 import "../css/ChangeLog.css"; // Importa los estilos CSS
 
 const ChangelogComponent = () => {
-    const [activeTab, setActiveTab] = useState("tab-0.6.0");
+    const [activeTab, setActiveTab] = useState("tab-0.7.0");
 
     const openTab = (event, tabId) => {
         event.preventDefault();
@@ -17,6 +17,14 @@ const ChangelogComponent = () => {
             <div className="changelog-container">
                 <div className="folder">
                     <div className="tabs">
+                        <button
+                            className={`tab ${activeTab === "tab-0.7.0" ? "active" : ""}`}
+                            onClick={(e) => openTab(e, "tab-0.7.0")}
+                        >
+                            <div>
+                                <span>Versión 0.6.0</span>
+                            </div>
+                        </button>
                         <button
                             className={`tab ${activeTab === "tab-0.6.0" ? "active" : ""}`}
                             onClick={(e) => openTab(e, "tab-0.6.0")}
@@ -68,6 +76,25 @@ const ChangelogComponent = () => {
                     </div>
 
                     <div className="content">
+                    <div id="tab-0.7.0" className={`content__inner ${activeTab === "tab-0.7.0" ? "active" : ""}`}>
+                            <div className="page">
+                                <ul>
+                                    <li>Se agrega conexión con la base de datos.</li>
+                                    <li>Se han actualizado los comandos existentes para mayor accesibilidad y rapidez.</li>
+                                    <li>Se ha agrega nuevos Sistemas:</li>
+                                    <ul>
+                                        <li>`-setup-bienvenida`: Configura el sistema de Bienvenida, personalizar el fondo y colores del mismo.</li>
+                                        <li>`-setup-tickets`: Configura el sistema de Tickets, define el canal para crear los tickets de los usuario.</li>
+                                        <li>`-setup-nivel`: Configura el sistema de Niveles, donde se puede definir en que canal se va a enviar un mensaje cuando el usuario sube de nivel.</li>
+                                    </ul>
+                                    <li>Comando `-nivel`, que muestra el nivel que tiene el usuario.</li>
+                                    <li>Se ha actualizado el sistema de ayuda donde el bot envía un mensaje al DM del usuario para solucionar los posibles problemas del mismo.</li>
+                                    <li>Se ha agregado el comando `-chiste`, donde se envía un chiste al azar.</li>
+                                    <li>Se ha agregado el comando `-8ball`, donde se envía una respuesta al azar de la pregunta.</li>
+                                </ul>
+                            </div>
+                        </div>
+
                         <div id="tab-0.6.0" className={`content__inner ${activeTab === "tab-0.6.0" ? "active" : ""}`}>
                             <div className="page">
                                 <ul>
